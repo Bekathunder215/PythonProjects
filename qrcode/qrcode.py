447c988367898bbe7f8c2bfd8ea9d48048c31c22
Ioannis Bekiaris <johnbekiaris13@gmail.com>
2023-12-14 13:14:20 +0100

    deleted buggy qrcode

--- qrcode/qrcode.py
+++
@@ -1,9 +0,0 @@
-#we install qrcode library and Image library
-import qrcode
-
-def makeqr(url):
-    image = qrcode.make(url)
-    type(img)
-    image.save("QRimage.png")
-
-makeqr("https://www.youtube.com/watch?v=xvFZjo5PgG0")
