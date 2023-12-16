4d65c1024c58356cdca562881ab9202044089b82
Bekathunder215 <johnbekiaris13@gmail.com>
2023-12-16 21:38:17 +0100

    added a model value

--- Carbrand.py
+++ Carbrand.py
@@ -1,8 +1,9 @@
 #this project is for string concatination
 #we want to create a string that says, my favorite car is ____
 car = input("I am you, what is your favorite car brand??\n -> ")
+car1 = input("and what about the model??\n -> ")
 
-print("My favorite car brand is " + car)
-print("My favorite car brand is {}".format(car))
-print(f"My favorite car brand is {car} and i like it a lot.")
+print("My favorite car brand is " + car + "and specifically the model " + car1)
+print("My favorite car brand is {} and specifically the model {}".format(car, car1))
+print(f"My favorite car brand is {car} and specifically the model {car1}")
 #this was a programm
