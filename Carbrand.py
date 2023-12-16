58fcc39d1acc47c0390a9a1f69898d6c0a963fe8
Bekathunder215 <johnbekiaris13@gmail.com>
2023-12-16 21:27:34 +0100

    changed the capitalisation

--- Carbrand.py
+++ Carbrand.py
@@ -2,6 +2,7 @@
 #we want to create a string that says, my favorite car is ____
 car = input("I am you, what is your favorite car brand??\n -> ")
 
-print("my favorite car brand is " + car)
-print("my favorite car brand is {}".format(car))
-print(f"my favorite car brand is {car}")
+print("My favorite car brand is " + car)
+print("My favorite car brand is {}".format(car))
+print(f"My favorite car brand is {car} and i like it a lot.")
+#this was a programm
